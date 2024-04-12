64d0e8c63f64217703a5a5812c143bed54c4dc33
Izaak Schroeder <izaak.schroeder@gmail.com>
2024-04-12 11:32:02 -0700

    fixup! try again

--- internal/hs/link.go
+++ internal/hs/link.go
@@ -7,18 +7,18 @@ package hs
 #cgo darwin,amd64 LDFLAGS: -L${SRCDIR}/../../dist/darwin-amd64/lib
 #cgo darwin,amd64 CFLAGS: -I${SRCDIR}/../../dist/darwin-amd64/include/hs
 
-#cgo linux,arm64 LDFLAGS: -L${SRCDIR}/../../dist/linux-arm64/lib
+#cgo linux,arm64 LDFLAGS: -L${SRCDIR}/../../dist/linux-arm64/lib -lc++ -lc++abi
 #cgo linux,arm64 CFLAGS: -I${SRCDIR}/../../dist/linux-arm64/include/hs
 
-#cgo linux,amd64 LDFLAGS: -L${SRCDIR}/../../dist/linux-amd64/lib
+#cgo linux,amd64 LDFLAGS: -L${SRCDIR}/../../dist/linux-amd64/lib -lc++ -lc++abi
 #cgo linux,amd64 CFLAGS: -I${SRCDIR}/../../dist/linux-amd64/include/hs
 
-#cgo windows,arm64 LDFLAGS: -L${SRCDIR}/../../dist/windows-arm64/lib
+#cgo windows,arm64 LDFLAGS: -L${SRCDIR}/../../dist/windows-arm64/lib -lc++
 #cgo windows,arm64 CFLAGS: -I${SRCDIR}/../../dist/windows-arm64/include/hs
 
-#cgo windows,amd64 LDFLAGS: -L${SRCDIR}/../../dist/windows-amd64/lib
+#cgo windows,amd64 LDFLAGS: -L${SRCDIR}/../../dist/windows-amd64/lib -lc++
 #cgo windows,amd64 CFLAGS: -I${SRCDIR}/../../dist/windows-amd64/include/hs
 
-#cgo LDFLAGS: -lhs -lc++ -lc++abi
+#cgo LDFLAGS: -lhs
 */
 import "C"
