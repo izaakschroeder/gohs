b7e74e4c83a43b306c54a58ada32fffbca2859a2
Izaak Schroeder <izaak.schroeder@gmail.com>
2024-05-14 13:51:32 -0700

    wip! test

--- internal/hs/link.go
+++ internal/hs/link.go
@@ -1,10 +1,10 @@
 package hs
 
 /*
-#cgo darwin,arm64 LDFLAGS: -L${SRCDIR}/../../dist/darwin-arm64/lib -lhs -lc++ -lc++abi
+#cgo darwin,arm64 LDFLAGS: -L${SRCDIR}/../../dist/darwin-arm64/lib -lhs -lc++abi
 #cgo darwin,arm64 CFLAGS: -I${SRCDIR}/../../dist/darwin-arm64/include/hs
 
-#cgo darwin,amd64 LDFLAGS: -L${SRCDIR}/../../dist/darwin-amd64/lib -lhs -lc++ -lc++abi
+#cgo darwin,amd64 LDFLAGS: -L${SRCDIR}/../../dist/darwin-amd64/lib -lhs -lc++abi
 #cgo darwin,amd64 CFLAGS: -I${SRCDIR}/../../dist/darwin-amd64/include/hs
 
 #cgo linux,arm64 LDFLAGS: -L${SRCDIR}/../../dist/linux-arm64/lib -lhs -lc++ -lc++abi
